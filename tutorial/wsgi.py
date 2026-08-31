d8e432b7988b23c654e3bcf7e1b0d82ebb5df68b
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-08-31 15:45:04 +0700

    refactor: change name of project to 'tutorial'

--- tutorial/wsgi.py
+++ tutorial/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tutorial_0 project.
+WSGI config for tutorial project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial_0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
 
 application = get_wsgi_application()
