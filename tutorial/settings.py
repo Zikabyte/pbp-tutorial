f68b72d99d9aa28a9cb6c571e73ed293443a4c9a
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-09-01 13:48:41 +0700

    feat: implement landing page html and styling

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'tutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,6 +71,9 @@ TEMPLATES = [
         },
     },
 ]
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 WSGI_APPLICATION = 'tutorial.wsgi.application'
 
