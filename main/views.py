5d8aa080ab81025bba5cd1019a2d52154ada95a5
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-09-09 14:59:22 +0700

    feat(tutorial): complete tutorial 02
    - implement experience model and template
    - migrate hardcoded personal data in index to a content based seed

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Mohammad Zidane Kurnianto",
+        "npm": "2506584861",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Baik itu membangun kode, maupun merusak kode, saya mempelajari dari keduanya. Saya tertarik dengan segala hal software engineering ddan AI engineering. "
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Burhan",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
