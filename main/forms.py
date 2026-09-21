49d63bf44f3d3f69487bb2434e13b5408bf01800
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-09-21 12:14:24 +0700

    feat(experience): implement ExperienceForm in forms.py

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm, TextInput, Textarea, URLInput, Select
+from django.forms import ModelForm, TextInput, Textarea, URLInput, Select, DateInput
 
-from main.models import Project
+from main.models import Experience, Project
 
 class ProjectForm(ModelForm):
     class Meta:
@@ -47,6 +47,56 @@ class ProjectForm(ModelForm):
             "category": Select(
                 attrs={
                     "class": "form-select",  # Opsional: tambahkan class CSS jika pakai Tailwind/Bootstrap
+                }
+            ),
+        }
+
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = Experience
+
+        fields = [
+            "title",
+            "description",
+            "category",
+            "thumbnail",
+            "ended_at",
+        ]
+
+        labels = {
+            "title": "Nama Pengalaman",
+            "description": "Deskripsi Pengalaman",
+            "category": "Kategori Pengalaman",
+            "thumbnail": "URL Gambar Pengalaman",
+            "ended_at": "Tanggal Selesai",
+        }
+
+        widgets = {
+            "title": TextInput(
+                attrs={
+                    "placeholder": "Asisten Dosen DDP-1",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Ceritakan Pengalamanmu",
+                    "rows": 3,
+                }
+            ),
+            "thumbnail": URLInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
+                }
+            ),
+            "category": Select(
+                attrs={
+                    "class": "form-select",
+                }
+            ),
+            "ended_at": DateInput(
+                attrs={
+                    "type": "date",
                 }
             ),
         }
