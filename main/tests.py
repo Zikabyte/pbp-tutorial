3fb645fb1e6b22ba7f91ddd626b14dc135c67f49
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-09-21 21:52:25 +0700

    feat: add test cases for new views (project and experience CRUD) and update claude code logs

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,5 @@
+import json
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.test import TestCase
 from django.urls import reverse
@@ -30,7 +32,7 @@ class MainTest(TestCase):
         self.assertTemplateUsed(response, "index.html")
         self.assertNotContains(response, self.experience.title)
         self.assertContains(response, f'href="{reverse("main:show_experience")}"')
-        self.assertContains(response, f'href="{reverse("main:show_project")}"')
+        self.assertContains(response, f'href="{reverse("main:show_projects")}"')
 
     def test_nonexistent_page_returns_404(self):
         response = self.client.get("/halaman-yang-tidak-ada/")
@@ -68,9 +70,99 @@ class MainTest(TestCase):
         self.assertContains(response, "Selesai")
         self.assertNotContains(response, "Sedang berlangsung")
 
+    # --------------------------- Experience CRUD Testing -------------------------- #
+    def test_create_experience_view_get(self):
+        response = self.client.get(reverse("main:create_experience"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "experience_form.html")
+
+    def test_create_experience_valid_post(self):
+        response = self.client.post(reverse("main:create_experience"), data={
+            "title": "Mobile Apps Developer Intern at McDonalds",
+            "description": "Mengembangkan mobile app.",
+            "category": "internship",
+        })
+
+        self.assertRedirects(response, reverse("main:show_experience"))
+        self.assertTrue(Experience.objects.filter(title="Mobile Apps Developer Intern at McDonalds").exists())
+
+    def test_create_experience_invalid_post_does_not_create(self):
+        initial_count = Experience.objects.count()
+        response = self.client.post(reverse("main:create_experience"), data={
+            "title": "",
+            "description": "",
+            "category": "internship",
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Experience.objects.count(), initial_count)
+
+    def test_update_experience_view_get_prefills_form(self):
+        response = self.client.get(reverse("main:update_experience", args=[self.experience.id]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "experience_form.html")
+        self.assertContains(response, self.experience.title)
+
+    def test_update_experience_valid_post(self):
+        response = self.client.post(
+            reverse("main:update_experience", args=[self.experience.id]),
+            data={
+                "title": "Asisten Dosen DDP-1",
+                "description": self.experience.description,
+                "category": self.experience.category,
+            },
+        )
+
+        self.assertRedirects(response, reverse("main:show_experience"))
+        self.experience.refresh_from_db()
+        self.assertEqual(self.experience.title, "Asisten Dosen DDP-1")
+
+    def test_update_nonexistent_experience_returns_404(self):
+        response = self.client.get(
+            reverse("main:update_experience", args=["00000000-0000-0000-0000-000000000000"])
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_experience_post_removes_it(self):
+        response = self.client.post(reverse("main:delete_experience", args=[self.experience.id]))
+
+        self.assertRedirects(response, reverse("main:show_experience"))
+        self.assertFalse(Experience.objects.filter(id=self.experience.id).exists())
+
+    def test_delete_experience_get_does_not_remove_it(self):
+        response = self.client.get(reverse("main:delete_experience", args=[self.experience.id]))
+
+        self.assertRedirects(response, reverse("main:show_experience"))
+        self.assertTrue(Experience.objects.filter(id=self.experience.id).exists())
+
+    def test_get_experience_json(self):
+        response = self.client.get(reverse("main:get_experience_json"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("application/json", response["Content-Type"])
+        data = json.loads(response.content)
+        titles = [entry["fields"]["title"] for entry in data]
+        self.assertIn(self.experience.title, titles)
+
+    def test_get_experience_json_filters_by_title(self):
+        Experience.objects.create(
+            title="Volunteer Mengajar",
+            description="Mengajar coding untuk anak-anak.",
+            category="volunteer",
+        )
+
+        response = self.client.get(reverse("main:get_experience_json"), {"title": "Asisten"})
+        data = json.loads(response.content)
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["fields"]["title"], self.experience.title)
+
     # ------------------------------ Project Testing ----------------------------- #
     def test_project_page(self):
-        response = self.client.get(reverse("main:show_project"))
+        response = self.client.get(reverse("main:show_projects"))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "project.html")
@@ -81,12 +173,107 @@ class MainTest(TestCase):
 
     def test_empty_project_page(self):
         Project.objects.all().delete()
-        response = self.client.get(reverse("main:show_project"))
+        response = self.client.get(reverse("main:show_projects"))
 
-        self.assertContains(response, "Belum ada project yang ditambahkan.")
+        self.assertContains(response, "Belum ada proyek yang ditambahkan.")
 
     def test_project_model(self):
             self.assertEqual(str(self.project), "Zikapedia")
+
+    # ---------------------------- Project CRUD Testing ---------------------------- #
+    def test_create_project_view_get(self):
+        response = self.client.get(reverse("main:create_project"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "projects_form.html")
+
+    def test_create_project_valid_post(self):
+        response = self.client.post(reverse("main:create_project"), data={
+            "name": "Samudera",
+            "description": "Aplikasi pemantau saham.",
+            "image_url": "/static/img/project-samudera.png",
+            "category": "mobile-development",
+            "project_url": "https://github.com/Zikabyte/samudera",
+        })
+
+        self.assertRedirects(response, reverse("main:show_projects"))
+        self.assertTrue(Project.objects.filter(name="Samudera").exists())
+
+    def test_create_project_invalid_post_does_not_create(self):
+        initial_count = Project.objects.count()
+        response = self.client.post(reverse("main:create_project"), data={
+            "name": "",
+            "description": "",
+            "category": "general",
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Project.objects.count(), initial_count)
+
+    def test_update_project_view_get_prefills_form(self):
+        response = self.client.get(reverse("main:update_project", args=[self.project.id]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "projects_form.html")
+        self.assertContains(response, self.project.name)
+
+    def test_update_project_valid_post(self):
+        response = self.client.post(
+            reverse("main:update_project", args=[self.project.id]),
+            data={
+                "name": "Zikapedia v2",
+                "description": self.project.description,
+                "image_url": "/static/img/project-zikapedia.png",
+                "category": self.project.category,
+                "project_url": self.project.project_url,
+            },
+        )
+
+        self.assertRedirects(response, reverse("main:show_projects"))
+        self.project.refresh_from_db()
+        self.assertEqual(self.project.name, "Zikapedia v2")
+
+    def test_update_nonexistent_project_returns_404(self):
+        response = self.client.get(
+            reverse("main:update_project", args=["00000000-0000-0000-0000-000000000000"])
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_project_post_removes_it(self):
+        response = self.client.post(reverse("main:delete_project", args=[self.project.id]))
+
+        self.assertRedirects(response, reverse("main:show_projects"))
+        self.assertFalse(Project.objects.filter(id=self.project.id).exists())
+
+    def test_delete_project_get_does_not_remove_it(self):
+        response = self.client.get(reverse("main:delete_project", args=[self.project.id]))
+
+        self.assertRedirects(response, reverse("main:show_projects"))
+        self.assertTrue(Project.objects.filter(id=self.project.id).exists())
+
+    def test_get_projects_json(self):
+        response = self.client.get(reverse("main:get_projects_json"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("application/json", response["Content-Type"])
+        data = json.loads(response.content)
+        names = [entry["fields"]["name"] for entry in data]
+        self.assertIn(self.project.name, names)
+
+    def test_get_projects_json_filters_by_title_param(self):
+        Project.objects.create(
+            name="Samudera",
+            description="Aplikasi pemantau saham.",
+            category="mobile-development",
+            project_url="https://github.com/Zikabyte/samudera",
+        )
+
+        response = self.client.get(reverse("main:get_projects_json"), {"title": "Zika"})
+        data = json.loads(response.content)
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["fields"]["name"], self.project.name)
 
 # ----------------- Bonus: Functional Testing w/ Selenium :) ----------------- #
 class NavbarFunctionalTest(StaticLiveServerTestCase):
@@ -110,7 +297,7 @@ class NavbarFunctionalTest(StaticLiveServerTestCase):
         self.assertIn(reverse("main:show_experience"), self.selenium.current_url)
 
         self.selenium.find_element(By.LINK_TEXT, "Project").click()
-        self.assertIn(reverse("main:show_project"), self.selenium.current_url)
+        self.assertIn(reverse("main:show_projects"), self.selenium.current_url)
 
         self.selenium.find_element(By.LINK_TEXT, "Profile").click()
         self.assertIn(reverse("main:show_main"), self.selenium.current_url)
